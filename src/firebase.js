061b0f6e38d3c0a077665731352a54050d630e1d
mishuk09 <mishukinfo09@gmail.com>
2025-02-05 16:12:00 +0600

    update firebase hosting

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBPap9S5DUGilJIBk3tiBDKoeXfYplmY-4",
+    authDomain: "e-commerce-4103d.firebaseapp.com",
+    projectId: "e-commerce-4103d",
+    storageBucket: "e-commerce-4103d.firebasestorage.app",
+    messagingSenderId: "58341495042",
+    appId: "1:58341495042:web:b5951921b99081f766f21b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
